ff3c71c5a48e2cbea31ef40f9f5086775ed34804
Kevin Botero <kbotero201@gmail.com>
2020-09-11 20:04:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,10 @@ def reverse_each_word(str)
   new_arr = arr.collect {|a| a.reverse}
   new_arr.join
 end
+
+def reverse_each_word(str)
+split_string = string.split("")
+  reversed = []
+  string.size.times { reversed << split_string.pop }
+  reversed.join
+end
