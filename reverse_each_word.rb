e2f4ca98a2c64d4f32457341bad47bc3279a2afa
Kevin Botero <kbotero201@gmail.com>
2020-09-11 20:01:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 def reverse_each_word(name)
   name.gsub(/\w+/) { |match| match.reverse }
 end
+
+def reverse_string str
+  arr = str.split /\b/
+  new_arr = arr.collect {|a| a.reverse}
+  new_arr.join
+end
